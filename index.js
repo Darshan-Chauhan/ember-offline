7e9773886b28cf6f47baf35a00d72f55d2774b32
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2019-02-14 12:06:22 +0530

    Commented

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-/* eslint-env node */
-'use strict';
+// /* eslint-env node */
+// 'use strict';
 
-const FastbootTransform = require('fastboot-transform');
+// const FastbootTransform = require('fastboot-transform');
 
-module.exports = {
-  name: 'ember-offline',
-  options: {
-    nodeAssets: {
-      'offline-js': {
-        vendor: {
-          include: ['offline.js', 'themes/*'],
-          processTree(input) {
-            return FastbootTransform(input);
-          }
-        }
-      }
-    }
-  },
-  included(app) {
-    this._super.included.apply(this, arguments);
-    this._ensureThisImport();
+// module.exports = {
+//   name: 'ember-offline',
+//   options: {
+//     nodeAssets: {
+//       'offline-js': {
+//         vendor: {
+//           include: ['offline.js', 'themes/*'],
+//           processTree(input) {
+//             return FastbootTransform(input);
+//           }
+//         }
+//       }
+//     }
+//   },
+//   included(app) {
+//     this._super.included.apply(this, arguments);
+//     this._ensureThisImport();
 
-    let config = this.project.config(app.env);
+//     let config = this.project.config(app.env);
 
-    let themes = config.emberOffline.themes;
-    let themesDir =  'vendor/offline-js/themes';
+//     let themes = config.emberOffline.themes;
+//     let themesDir =  'vendor/offline-js/themes';
 
-    if(!themes) {
-      themes = {
-        theme: 'default',
-        indicator: false,
-        language: 'english',
-      }
-    }
+//     if(!themes) {
+//       themes = {
+//         theme: 'default',
+//         indicator: false,
+//         language: 'english',
+//       }
+//     }
 
-    if (themes.theme) {
-      this.import(`${themesDir}/offline-theme-${themes.theme}.css`);
-      this.import(`${themesDir}/offline-language-${themes.language}.css`);
-    }
+//     if (themes.theme) {
+//       this.import(`${themesDir}/offline-theme-${themes.theme}.css`);
+//       this.import(`${themesDir}/offline-language-${themes.language}.css`);
+//     }
 
-    if (themes.indicator) {
-      this.import(`${themesDir}/offline-theme-${themes.indicator}-indicator.css`);
-      this.import(`${themesDir}/offline-language-${themes.language}-indicator.css`);
-    }
+//     if (themes.indicator) {
+//       this.import(`${themesDir}/offline-theme-${themes.indicator}-indicator.css`);
+//       this.import(`${themesDir}/offline-language-${themes.language}-indicator.css`);
+//     }
 
-    this.import('vendor/offline-js/offline.js');
-  },
-  _ensureThisImport() {
-    if (!this.import) {
-      this._findHost = function findHostShim() {
-        let current = this;
-        let app;
+//     this.import('vendor/offline-js/offline.js');
+//   },
+//   _ensureThisImport() {
+//     if (!this.import) {
+//       this._findHost = function findHostShim() {
+//         let current = this;
+//         let app;
 
-        do {
-          app = current.app || app;
-        } while (current.parent.parent && (current = current.parent));
-        return app;
-      };
-      this.import = function importShim(asset, options) {
-        let app = this._findHost();
-        app.import(asset, options);
-      };
-    }
-  }
-};
+//         do {
+//           app = current.app || app;
+//         } while (current.parent.parent && (current = current.parent));
+//         return app;
+//       };
+//       this.import = function importShim(asset, options) {
+//         let app = this._findHost();
+//         app.import(asset, options);
+//       };
+//     }
+//   }
+// };
